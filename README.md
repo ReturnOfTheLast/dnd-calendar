e8ed08b35f82520368867b2e705067b42d46a356
ReturnOfTheLast <returnofthelast@gmail.com>
2021-11-15 16:57:33 +0100

    First Commit: Project setup and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# DnD Calendar
+This is a simple django website that I plan to use to show and calculate dates in my dnd games
---
+++ dcalendar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DcalendarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dcalendar'
